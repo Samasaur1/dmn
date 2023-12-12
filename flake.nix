3e6733bea68f6e738fea336441badb9c88f3cbb7
Sam <30577766+Samasaur1@users.noreply.github.com>
2023-12-11 20:38:55 -0800

    Nixify

---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  description = "A Discord bot that watches RSS feeds";
+
+  outputs = { nixpkgs, ... }:
+    let
+      forAllSystems = gen:
+        nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed
+        (system: gen nixpkgs.legacyPackages.${system});
+    in {
+      packages = forAllSystems (pkgs: { default = pkgs.callPackage ./. { }; });
+    };
+}
