22efbb5f015b4d0391b7a03785e98dead76be01f
Sam <30577766+Samasaur1@users.noreply.github.com>
2024-01-04 19:46:52 +0100

    Actually enable LaunchAgent

--- module.nix
+++ module.nix
@@ -26,25 +26,28 @@ in
     assertions = [
       (hm.assertions.assertPlatform "targets.darwin.defaults" pkgs platforms.darwin)
     ];
-    launchd.agents.dmn.config = {
-      Label = "com.gauck.sam.dmn";
-      KeepAlive = true;
-      ProgramArguments =
-        let
-          package = pkgs.callPackage ./. {};
-        in
-        [
-          "${package}/bin/dmn"
-        ];
-      # Theoretically you can view logs directly with
-      # `sudo launchctl debug <service> --stdout --stderr`
-      # As is typical for launchd, this has never worked for me,
-      # so possibly include an option to enable log files in the future?
-      # Or just expect people to set
-      # `launchd.user.agents.dmn.serviceConfig.StandardOutPath = "/tmp/out";`
-      # on their own.
-      # StandardOutPath = "/tmp/nix-dmn-stdout.log";
-      # StandardErrorPath = "/tmp/nix-dmn-stderr.log";
+    launchd.agents.dmn = {
+      enable = true;
+      config = {
+        Label = "com.gauck.sam.dmn";
+        KeepAlive = true;
+        ProgramArguments =
+          let
+            package = pkgs.callPackage ./. {};
+          in
+          [
+            "${package}/bin/dmn"
+          ];
+        # Theoretically you can view logs directly with
+        # `sudo launchctl debug <service> --stdout --stderr`
+        # As is typical for launchd, this has never worked for me,
+        # so possibly include an option to enable log files in the future?
+        # Or just expect people to set
+        # `launchd.user.agents.dmn.serviceConfig.StandardOutPath = "/tmp/out";`
+        # on their own.
+        # StandardOutPath = "/tmp/nix-dmn-stdout.log";
+        # StandardErrorPath = "/tmp/nix-dmn-stderr.log";
+      };
     };
     xdg.configFile."dmn/commands.json".text = (builtins.toJSON cfg.commands);
   };
